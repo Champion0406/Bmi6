02fb3f6885be8fdbcc872edb7845593431277521
champion0406 <barney0406@gmail.com>
2017-10-31 12:16:59 +0800

    first commit

---
+++ app/src/main/java/com/champion/bmi6/MainActivity.java
@@ -0,0 +1,22 @@
+package com.champion.bmi6;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void bmi(View view){
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+
+
+    }
+}
